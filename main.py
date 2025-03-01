8d2794fa2bd9c1d0d907d72e1d0409ba4b8a08a7
qmueller <quentin.muller@epfl.ch>
2025-03-01 13:43:27 +0100

    Grab chapters

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-with open('urls.txt') as f:
-    lines = f.read().splitlines()
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
 
-with open("test.txt", "w") as f :
-    for line in lines :
-        f.write(f"{line}\n")
+urls = []
+with open('urls.txt') as f:
+    urls = f.read().splitlines()
+
+last_chapters = []
+
+for url in urls :
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html_page = urlopen(req).read()
+    soup = BeautifulSoup(html_page, 'html.parser')
+    soup_str = str(soup)
+    reg = re.compile('Chapter [1-9]+')
+    chap_list = [float(x.split()[-1]) for x in reg.findall(soup_str)]
+    last_chapter = max(chap_list)
+    last_chapters.append(int(last_chapter) if last_chapter.is_integer() else last_chapter)
+
+with open("last_chapters.txt", "w") as f:
+    for l in last_chapters :
+        f.write(f"{l}\n")
